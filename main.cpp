3bbfb41a1b196d2634b2359f5b8216a86aa8d2bd
bfigueroa99 <bfigueroa@miuandes.cl>
2022-09-08 17:02:28 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+using namespace std;
+
+// void imprimir(){
+
+// }
+
+int Inserccion(array_temp,size_of_array){
+    int *n[size_of_array];
+    return *n;
+}
+
+int main(){
+    cout<<"hola"<<endl;
+    return 0;
+}
+
