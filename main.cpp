160fcb34b9fb51e5680ab7bdb3484d77d02c725c
bfigueroa99 <bfigueroa@miuandes.cl>
2022-09-09 15:46:03 -0400

    selection ready

--- main.cpp
+++ main.cpp
@@ -7,13 +7,13 @@ using namespace std;
 int main(int argc, char **argv){
 
     //crear arreglo aleatorio
-    int size_of_array = 10;
+    int size_of_array = 100000; //tamaño del arreglo
     float array[size_of_array];
     srand(time(NULL));
    
     //llenar arreglo
     for(int i=0;i<=size_of_array;i++){
-        array[i]=rand()%100;
+        array[i]=rand()%100; //entre que numeros quiere que varien
     }
 
     //imprimir arreglo
@@ -21,9 +21,10 @@ int main(int argc, char **argv){
         cout<<array[i]<<" ";
     }
 
+    //---------------------------------------------------------------------------------------------
+    //Inserccion
     int temp;
     int j;
-    //Inserccion (lo tengo como las weas)(no funciona)
     for(int i=0;i<size_of_array;i++){
         temp = array[i];
         j = i-1;
@@ -34,8 +35,25 @@ int main(int argc, char **argv){
         array[j+1] = temp;
     }
     cout<<"\n\n"<<endl;
+    //---------------------------------------------------------------------------------------------------
 
+    //------------------------------------------------------------------------------------------------------
     //Seleccion
+    temp=0;
+    int small;
+    for(int i=0;i<size_of_array;i++){
+        small = i;
+        for(j=i;j<size_of_array;j++){
+            if(array[j]<array[small]){
+                small = j;
+            }
+        }
+        temp = array[i];
+        array[i] = array[small];
+        array[small] = temp;
+    }
+    //------------------------------------------------------------------------------------------------------
+
 
     //QuickSort
 
