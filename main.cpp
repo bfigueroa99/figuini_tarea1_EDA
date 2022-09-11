03351d7090123a35295704a520f4a641d7fe51d0
St3fan1n1 <spromanini@miuandes.cl>
2022-09-11 17:32:25 -0300

    Passed all algorithms to functions

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ using namespace std;
 int split_qs(int i, int j, float* array);
 void QuickSort(float* array, int i, int j);
 void PrintArray(float* array, int size);
+void InsertionSort(float* array, int size);
+void SelectionSort(float* array, int size_of_array);
+void MergeSort(float* array, int size);
 
 int main(int argc, char **argv){
 
@@ -14,7 +17,7 @@ int main(int argc, char **argv){
     unsigned t0, t1, t2, t3;
 
     //crear arreglo aleatorio
-    int size_of_array = 11; //tamaño del arreglo
+    int size_of_array = 100; //tamaño del arreglo
     float array[size_of_array];
     srand(time(NULL));
    
@@ -24,71 +27,80 @@ int main(int argc, char **argv){
     }
 
     //imprimir arreglo
-    // PrintArray(array, size_of_array);
+    /* PrintArray(array, size_of_array); */
     
     cout << endl << endl;
 
-/* TODO:(stefano): Make it into a function*/
-    //---------------------------------------------------------------------------------------------
-    //Inserccion
-    //t0=clock() //parte el tiempo
-    /* int temp; */
-    /* int j; */
-    /* for(int i=0;i<size_of_array;i++){ */
-    /*     temp = array[i]; */
-    /*     j = i-1; */
-    /*     while(j>=0 && array[j]>temp){ */
-    /*         array[j+1]=array[j]; */
-    /*         j--; */
-    /*     } */
-    /*     array[j+1] = temp; */
-    /* } */
-    /* cout<<"\n\n"<<endl; */
-    //t1 = clock(); //termina el tiempo
-    //double time = (double(t1-t0)/CLOCKS_PER_SEC);
-    //cout << "Inserccion Time: " << time << endl;
     //---------------------------------------------------------------------------------------------------
-
-/* TODO:(stefano): Make it into a function*/
-    //------------------------------------------------------------------------------------------------------
-    //Seleccion
-    //t2=clock() //parte el tiempo
-    /* temp=0; */
-    /* int small; */
-    /* for(int i=0;i<size_of_array;i++){ */
-    /*     small = i; */
-    /*     for(j=i;j<size_of_array;j++){ */
-    /*         if(array[j]<array[small]){ */
-    /*             small = j; */
-    /*         } */
-    /*     } */
-    /*     temp = array[i]; */
-    /*     array[i] = array[small]; */
-    /*     array[small] = temp; */
-    /* } */
-    //t3 = clock(); //termina el tiempo
-    //double time = (double(t3-t2)/CLOCKS_PER_SEC);
-    //cout t<< "Saleccion Time: " << time << endl;
-    //------------------------------------------------------------------------------------------------------
 
     int i = 0;
     int j = size_of_array;
     
 /* WARN:(stefano): Commented functions not implemented yet*/
-    /* InsertionSort(); */
-    /* SelectionSort(); */
-    // QuickSort(array, i, j);
-    /* MergeSort(); */
+    clock_t start = clock();
+    /* InsertionSort(array, size_of_array); */
+    /* SelectionSort(array, size_of_array); */
+    /* QuickSort(array, i, j); */
+    MergeSort(array, size_of_array);
+    cout << "Time taken: " << ((double)(clock() - start) / CLOCKS_PER_SEC);
+    
+    /* PrintArray(array, size_of_array); */
+    
+    return 0;
+}
 
-    PrintArray(array, size_of_array);
-    cout<<"\n";
-    //--------------------------------------------------------------------------------------
-    //MergeSort(no funciona)
-    float array_tmp[size_of_array];
+void PrintArray(float* array, int size)
+{
+    for(int i = 0; i < size; i++){
+        cout<<array[i]<<" ";
+    }
+}
+
+void InsertionSort(float* array, int size)
+{
+    int temp;
+    int j;
+    for(int i=0;i<size;i++){
+        temp = array[i];
+        j = i-1;
+        while(j>=0 && array[j]>temp){
+            array[j+1]=array[j];
+            j--;
+        }
+        array[j+1] = temp;
+    }
+}
+
+void SelectionSort(float* array, int size_of_array)
+{
+    int temp=0;
+    int small;
+    for(int i=0;i<size_of_array;i++){
+        small = i;
+        for(int j=i;j<size_of_array;j++){
+            if(array[j]<array[small]){
+                small = j;
+            }
+        }
+        temp = array[i];
+        array[i] = array[small];
+        array[small] = temp;
+    }
+}
+
+void MergeSort(float* array, int size)
+{
+    float array_tmp[size];
     int q = 1;
     int p1 = 0;
-    int k = size_of_array/2;
+    int k = size/2;
     int p2 = k+1;
+    int j = size;
+    for(int i = 0; i < size; i++)
+    {
+        array_tmp[i] = 0;
+    }
+    cout << sizeof(array_tmp) << endl;
     while((p1<=k)&&(p2<=j)){
         if(array[p1]<=array[p2]){
             array_tmp[q] = array[p1];
@@ -110,21 +122,8 @@ int main(int argc, char **argv){
         p2++;
         q++;
     }   
-    for(int i=0;i<=size_of_array;i++){
+    for(int i=0;i<=size;i++){
         array[i]=array_tmp[i]; //entre que numeros quiere que varien
-    }
-
-    //---------------------------------------------------------------------------------------
-    cout<<"\n";
-    PrintArray(array, size_of_array);
-
-    return 0;
-}
-
-void PrintArray(float* array, int size)
-{
-    for(int i = 0; i < size; i++){
-        cout<<array[i]<<" ";
     }
 }
 
